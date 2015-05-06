0bde8a92559461ec531abe3c25f3b2768db02057
Ariel Mashraki <ariel@mashraki.co.il>
2015-05-06 15:06:47 +0300

    lexer regex

--- lexer.go
+++ lexer.go
@@ -1,23 +1,86 @@
 package mark
 
-// Token represent token or string returned from the lexer(scanner)
-type Token struct {
-	Type Type   // The type of this item
-	Line int    // The line number on thich this token apears
-	Text string // The text of this item
+import (
+	"go/token"
+)
+
+// itemType identifies the type of lex items.
+type itemType int
+
+// Item represent a token or text string returned from the scanner
+type item struct {
+	typ itemType  // The type of this item.
+	pos token.Pos // The starting position, in bytes, of this item in the input string.
+	val string    // The value of this item.
 }
 
-// Type identifies the type of lex items
-type Type int
-
 const (
-	EOF   Type = iota // Zero value so closed channel delivers EOF
-	Error             // Error occurred; value is text of error
-	NewLine
+	itemEOF   itemType = iota - 1 // Zero value so closed channel delivers EOF
+	itemError                     // Error occurred; value is text of error
 	// Intersting things
-	Paragraph // Simple paragraph
-	Heading   // Simple heading
-	LHeading  // Line heading
-	Hr        // Thematic break
-
+	itemNewLine
+	itemHTML
+	// Block Elements
+	itemParagraph
+	itemLineBreak
+	itemHeading
+	itemLHeading
+	itemBlockQuote
+	itemList
+	itemCodeBlock
+	itemHr
+	itemTable
+	// Span Elements
+	itemLinks
+	itemEmphasis
+	itemCode
+	itemImages
 )
+
+// Regexp grammer
+const (
+	code      = "/^`/"
+	codeBlock = "/^```/"
+	heading   = "/^#/"
+	lheading  = "/^([^\n]+)\n *(=|-){2,} *(?:\n+|$)/"
+	comment   = "/<!--[\\s\\S]*?-->/"
+)
+
+// stateFn represents the state of the scanner as a function that returns the next state.
+type stateFn func(*lexer) stateFn
+
+// lexer holds the state of the scanner.
+type lexer struct {
+	name       string    // the name of the input; used only for error reports
+	input      string    // the string being scanned
+	state      stateFn   // the next lexing function to enter
+	posPos     token.Pos // current position in the input
+	start      token.Pos // start position of this item
+	width      token.Pos // width of last rune read from input
+	lastPos    token.Pos // position of most recent item returned by nextItem
+	items      chan item // channel of scanned items
+	parenDepth int       // nesting depth of ( ) exprs
+}
+
+// lex creates a new lexer for the input string.
+func lex(name, input string) *lexer {
+	l := &lexer{
+		name:  name,
+		input: input,
+		items: make(chan item),
+	}
+	go l.run()
+	return l
+}
+
+// run runs the state machine for the lexer.
+func (l *lexer) run() {
+	for l.state = lexAny; l.state != nil; {
+		l.state = l.state(l)
+	}
+}
+
+// lexAny scans non-space items.
+func lexAny(l *lexer) stateFn {
+
+}
--- parser.go
+++ parser.go
@@ -1,1 +1,22 @@
 package mark
+
+type Tree struct {
+	text string
+	lex  *lexer
+}
+
+func Parse(text string) (err error) {
+	// Create TreeSet
+	t := &Tree{text, new(lexer)}
+	err = t.Parse()
+	return
+}
+
+func (t *Tree) Parse() error {
+	t.parse()
+	return nil
+}
+
+func (t *Tree) parse() {
+
+}
