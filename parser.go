cf7ae6a96b63bd83b730e24fcfc8556f73ddd298
Ariel Mashraki <ariel@mashraki.co.il>
2015-05-16 22:53:05 +0300

    fix(parser): parseParagraph

--- parser.go
+++ parser.go
@@ -1,11 +1,31 @@
 package mark
 
+import (
+	"fmt"
+)
+
 type Tree struct {
 	text string
 	lex  *lexer
 	// Parsing only
 	token     [3]item // three-token lookahead for parser
 	peekCount int
+	Nodes     []Node
+}
+
+// Parse convert the raw text to NodeTree.
+func (t *Tree) parse() {
+Loop:
+	for {
+		switch p := t.peek().typ; p {
+		case eof:
+			break Loop
+		case itemText, itemStrong, itemItalic, itemStrike, itemCode:
+			t.parseParagraph()
+		default:
+			fmt.Println("Nothing to do")
+		}
+	}
 }
 
 // next returns the next token
@@ -26,4 +46,14 @@ func (t *Tree) peek() item {
 	t.peekCount = 1
 	t.token[0] = t.lex.nextItem()
 	return t.token[0]
+}
+
+// parseParagraph scan until itemBr occur.
+func (t *Tree) parseParagraph() {
+	// New paragraph node
+
+	// for loop
+	// if eof;
+	// if br; append to tree
+	// always text... and push emphasis to it.
 }
