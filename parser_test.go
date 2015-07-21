d89d6afa7c54381316475fb9a749e715da343f5f
Ariel Mashraki <ariel@mashraki.co.il>
2015-07-21 17:22:56 +0300

    test(parser): basic parse testing

--- parser_test.go
+++ parser_test.go
@@ -1,95 +1,110 @@
 package mark
 
-// import (
-// 	"fmt"
-// 	"github.com/k0kubun/pp"
-// 	"testing"
-// )
+import (
+	"testing"
+)
 
-// func jTestParser(t *testing.T) {
-// 	l := lex("1", "foo bar baz\nhello world")
-// 	p := &Tree{lex: l}
-// 	fmt.Println("peek")
-// 	item := p.peek()
-// 	fmt.Println(tokenNames[item.typ], "-->", item.val)
-// 	item = p.peek()
-// 	fmt.Println(tokenNames[item.typ], "-->", item.val)
-// 	fmt.Println("next")
-// 	item = p.next()
-// 	fmt.Println(tokenNames[item.typ], "-->", item.val)
-// }
+type parseTest struct {
+	name  string
+	items []item
+	nodes []NodeType
+}
 
-// func ParseFn(*testing.T) {
-// 	l := lex("2", "hello\nworld. **ariel**foo  \nenter hahaha  \n~~hello~~ world  \n_bar_  \n This is my code:`javascript`")
-// 	p := &Tree{lex: l}
-// 	p.parse()
+type mockLexer struct {
+	items []item
+}
 
-// 	pp.Printf("[Message]: Tree Node List After Compile\n\n")
-// 	pp.Println(p.Nodes)
-// 	pp.Println("Length of nodes:", len(p.Nodes))
-// 	p.render()
-// 	pp.Printf(p.output + "\n")
+func (l *mockLexer) nextItem() (t item) {
+	if len(l.items) == 0 {
+		return item{itemEOF, 0, ""}
+	}
+	t, l.items = l.items[0], l.items[1:]
+	return
+}
 
-// 	l = lex("3", "```js\nMy Code Block\nbla bla\n```\n    block code    \n    yeah with more rows    \n")
-// 	p = &Tree{lex: l}
-// 	p.parse()
-// 	p.render()
-// 	pp.Printf("\n" + p.output + "\n")
+func NewMockLex(items []item) *mockLexer {
+	return &mockLexer{items: items}
+}
 
-// 	l = lex("4", "fooo  \n***\nafter hr")
-// 	p = &Tree{lex: l}
-// 	p.parse()
-// 	p.render()
-// 	pp.Printf("\n" + p.output + "\n")
+var blockParseTests = []parseTest{
+	{"eof", []item{}, []NodeType{}},
+	{"text-1",
+		[]item{item{itemText, 0, "hello"}},
+		[]NodeType{NodeParagraph},
+	},
+	{"text-2",
+		[]item{
+			item{itemText, 0, "hello"},
+			item{itemNewLine, 0, "\n"},
+			item{itemText, 0, "world"},
+		},
+		[]NodeType{NodeParagraph},
+	},
+	{"text-3",
+		[]item{
+			item{itemText, 0, "hello"},
+			item{itemNewLine, 0, "\n"},
+			item{itemNewLine, 0, "\n\n"},
+			item{itemText, 0, "world"},
+		},
+		[]NodeType{NodeParagraph, NodeNewLine, NodeNewLine, NodeParagraph},
+	},
+	{"header",
+		[]item{
+			item{itemHeading, 0, "# Hello"},
+		},
+		[]NodeType{NodeHeading},
+	},
+	{"code-block",
+		[]item{
+			item{itemCodeBlock, 0, "    js\n    hello"},
+		},
+		[]NodeType{NodeCode},
+	},
+	{"table",
+		[]item{
+			item{itemTable, 0, ""},
+		},
+		[]NodeType{NodeTable},
+	},
+	{"list",
+		[]item{
+			item{itemList, 0, "-"},
+			item{itemListItem, 0, "hello"},
+		},
+		[]NodeType{NodeList},
+	},
+	{"HTML",
+		[]item{
+			item{itemHTML, 0, "<hello>\nworld</hello>"},
+		},
+		[]NodeType{NodeHTML},
+	},
+}
 
-// 	l = lex("5", "#foo bar")
-// 	p = &Tree{lex: l}
-// 	p.parse()
-// 	p.render()
-// 	pp.Printf("\n" + p.output + "\n")
+func collectNodes(t *parseTest) []Node {
+	tr := &Tree{lex: NewMockLex(t.items), links: make(map[string]*DefLinkNode)}
+	tr.parse()
+	return tr.Nodes
+}
 
-// 	l = lex("6", `
-// this is header
-// ===
-// And then we have some dummy text...
+func equalTypes(n1 []Node, n2 []NodeType) bool {
+	if len(n1) != len(n2) {
+		return false
+	}
+	for i := range n1 {
+		if n1[i].Type() != n2[i] {
+			return false
+		}
+	}
+	return true
+}
 
-// ## This is H2!!!
-// `)
-// 	p = &Tree{lex: l}
-// 	p.parse()
-// 	p.render()
-// 	pp.Printf("\n" + p.output + "\n")
-
-// 	l = lex("7", `
-// paragraph
-// <http://autolink.com>
-// [text](http://localhost.com "Ariel")
-// [text](http://google.com)
-// done
-// https://github.link done!
-// `)
-// 	p = &Tree{lex: l}
-// 	p.parse()
-// 	p.render()
-// 	pp.Printf("\n" + p.output + "\n")
-
-// 	l = lex("8", `
-// ![name](http://github.com/foo.gif "Title")
-// ![name](http://only-url)
-// paragraph
-// `)
-// 	p = &Tree{lex: l}
-// 	p.parse()
-// 	p.render()
-// 	pp.Printf("\n" + p.output + "\n")
-
-// 	l = lex("8", `
-// - This is listItem
-// - And another listItem
-// `)
-
-// 	for item := range l.items {
-// 		pp.Println(item)
-// 	}
-// }
-//
+func TestBlocksParse(t *testing.T) {
+	for _, test := range blockParseTests {
+		nodes := collectNodes(&test)
+		if !equalTypes(nodes, test.nodes) {
+			t.Errorf("%s: got\n\t%+v\nexpected\n\t%+v", test.name, nodes, test.nodes)
+		}
+	}
+}
