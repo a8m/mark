b6a6fe64fa8c4a2c4d39b68afce455957b845e7d
Ariel Mashraki <ariel@mashraki.co.il>
2015-08-17 08:52:26 +0300

    fix(parser): commonmark - 166

--- parser.go
+++ parser.go
@@ -223,8 +223,9 @@ func (p *parse) parseDefLink() *DefLinkNode {
 	// name(lowercase), href, title
 	n := p.newDefLink(token.pos, name, match[2], match[3])
 	// store in links
-	if _, ok := p.links[name]; !ok {
-		p.links[name] = n
+	links := p.root().links
+	if _, ok := links[name]; !ok {
+		links[name] = n
 	}
 	return n
 }
